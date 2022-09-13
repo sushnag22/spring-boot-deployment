053fdd3e249b087cacf415595f4e2cf19ec90e88
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-09-13 10:55:58 +0530

    Initial commit

---
+++ src/main/java/tech/sushnag22/springbootdeployment/SpringBootDeploymentApplication.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.springbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/sushnag22/springbootdeployment/SpringBootDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.springbootdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
