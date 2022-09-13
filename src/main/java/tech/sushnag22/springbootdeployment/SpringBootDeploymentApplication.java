92c8d43c28fd895a38f66cc62e17c953b15c3827
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-09-13 11:00:18 +0530

    Add @RestController annotation to make the application available for use by Spring MVC to handle web requests and @RequestMapping annotation to map "/" endpoint to helloDocker method which sends a "Hello, Docker!" response.

--- src/main/java/tech/sushnag22/springbootdeployment/SpringBootDeploymentApplication.java
+++ src/main/java/tech/sushnag22/springbootdeployment/SpringBootDeploymentApplication.java
@@ -2,9 +2,18 @@ package tech.sushnag22.springbootdeployment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDeploymentApplication {
+
+	@RequestMapping("/")
+	public String helloDocker() {
+		return "Hello, Docker!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDeploymentApplication.class, args);
